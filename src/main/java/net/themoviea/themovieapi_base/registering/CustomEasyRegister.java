1a5c38c28cfe67b999cae9e597412c63296a85d0
Themoviea <themoviea@gmail.com>
2021-02-24 21:37:37 +0900

    stuff i committed

--- src/main/java/net/themoviea/themovieapi_base/registering/CustomEasyRegister.java
+++ src/main/java/net/themoviea/themovieapi_base/registering/CustomEasyRegister.java
@@ -1,9 +1,18 @@
 package net.themoviea.themovieapi_base.registering;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import net.minecraft.util.registry.Registry;
 
-public interface CustomEasyRegister {
-	boolean createCustomRegisterList(Object... a);
+public interface CustomEasyRegister<ObjectClass> {
+	ArrayList<Object> getArrayList();
+	
+	default boolean createCustomRegisterList(Object... a) {
+		List<Object> list = Arrays.asList(a);
+		return getArrayList().addAll(list);
+	}
 	
 	void registerCustom(String modid, Registry<?> registry);
 }
