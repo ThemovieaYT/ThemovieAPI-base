4f851be42d3c2c1d14a32f7caef42b9c463dfd8a
Themoviea <themoviea@gmail.com>
2021-02-24 21:29:28 +0900

    no need for method to create custom list

--- src/main/java/net/themoviea/themovieapi_base/registering/CustomEasyRegister.java
+++ src/main/java/net/themoviea/themovieapi_base/registering/CustomEasyRegister.java
@@ -3,7 +3,6 @@ package net.themoviea.themovieapi_base.registering;
 import net.minecraft.util.registry.Registry;
 
 public interface CustomEasyRegister {
-	boolean createCustomRegisterList(Object... a);
 	
 	void registerCustom(String modid, Registry<?> registry);
 }
