4f36a853d77e8b66b34ea87542f8825c8f28cefc
Themoviea <themoviea@gmail.com>
2021-02-25 10:14:15 +0900

    Added more necessary code and documentation

--- src/main/java/net/themoviea/themovieapi_base/registering/CustomEasyRegister.java
+++ src/main/java/net/themoviea/themovieapi_base/registering/CustomEasyRegister.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.themoviea.themovieapi_base.exceptions.InputNotAnObjectException;
 
@@ -11,17 +13,27 @@ import net.themoviea.themovieapi_base.exceptions.InputNotAnObjectException;
  * Mainly an interface used for other APIs
  * to support EasyRegister so users can easily
  * register their own custom registries.
- * @author 1
+ * @author Themoviea
  *
  * @param <ObjectClass>
  */
 public interface CustomEasyRegister<ObjectClass> {
 	
 	/**
-	 * Gets the ArrayList from the class that implements this interface,
-	 * so no need to write createCustomRegisterList multiple times because
-	 * it is a default method.
-	 * @return
+	 * Gets the mod id.
+	 * @return ObjectClass
+	 */
+	String getModId();
+	
+	/**
+	 * Gets the object.
+	 * @return ObjectClass
+	 */
+	ObjectClass getObject();
+	
+	/**
+	 * Gets the ArrayList.
+	 * @return ArrayList<Object>
 	 */
 	ArrayList<Object> getArrayList();
 	
@@ -35,5 +47,20 @@ public interface CustomEasyRegister<ObjectClass> {
 	 * @param modid
 	 * @param registry
 	 */
-	void registerCustom(String modid, Registry<?> registry) throws InputNotAnObjectException;
+	@SuppressWarnings("unchecked")
+	default void registerCustom(String modid, Registry<ObjectClass> registry) throws InputNotAnObjectException {
+		if(getArrayList().size() == 0) {
+			System.out.println("(" + getModId() + ") No custom entries has been registered, as of there is nothing to register.");
+		} else {
+			for(int x = 0; x < getArrayList().size(); x = x + 2) {
+				if(getArrayList().get(x).equals(getObject())) {
+					if(getArrayList().get(x+1) instanceof String) {
+						Registry.register(registry, new Identifier(modid, (String)getArrayList().get(x+1)), (ObjectClass)getArrayList().get(x));
+					} else
+						throw new InputNotAnObjectException("The " + getArrayList().get(x+1) + "on index: " + x+1 + " is not a String!");
+				} else
+					throw new InputNotAnObjectException("The " + getArrayList().get(x) + "on index: " + x + " is not a custom entry from " + getModId() + "!");
+			}
+		}
+	}
 }
