2900250df13447a8b2e96b87230dd83ba4b21c79
Themoviea <themoviea@gmail.com>
2021-02-25 17:28:20 +0900

    Remember, API's shouldn't have content!

--- src/main/java/net/themoviea/themovieapi_base/ThemovieAPI.java
+++ src/main/java/net/themoviea/themovieapi_base/ThemovieAPI.java
@@ -12,18 +12,8 @@ import net.themoviea.themovieapi_base.registering.EasyRegister;
 
 public class ThemovieAPI implements ModInitializer
 {
-    public static final Block ENCHANTED_PLANKS = new Block(FabricBlockSettings.of(Material.WOOD, MaterialColor.BROWN_TERRACOTTA).hardness(2.0f).resistance(2.0f).sounds(BlockSoundGroup.WOOD));
-    public static final Block DARK_STONE = new Block(FabricBlockSettings.of(Material.STONE, MaterialColor.BLACK).hardness(3.5f).resistance(6.0f).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2));
-
+    
 	@Override
 	public void onInitialize() {
-		EasyRegister.createBlockList(DARK_STONE, "dark_stone", ENCHANTED_PLANKS, "enchanted_planks");
-		try {
-			EasyRegister.registerBlocks("testmod");
-		} catch (InputNotAnObjectException e) {
-			e.printStackTrace();
-			System.exit(1);
-			System.out.println();
-		}
 	}
 }
